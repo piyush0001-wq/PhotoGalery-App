d4e5ef62651ce244fd21f18fb8809619bb820037
piyush solakhiya <64743126+piyush0001-wq@users.noreply.github.com>
2021-05-06 11:17:36 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@ import 'firebase/firestore';
 
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCmxzJjeOWcC9qOyhmvem-HeLwOahYXOMI",
-  authDomain: "photogallery-8af5b.firebaseapp.com",
-  projectId: "photogallery-8af5b",
-  storageBucket: "photogallery-8af5b.appspot.com",
-  messagingSenderId: "663744057824",
-  appId: "1:663744057824:web:6b28e2419a9d5b7479db85"
+//your firebase api//
 });
 
 
